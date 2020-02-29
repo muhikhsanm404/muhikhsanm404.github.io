21780ab509d19532ed0d6c5860ae0d82570fb3fe
Mr.54nDr0X404 <44969167+muhikhsanm404@users.noreply.github.com>
2020-02-29 10:15:02 +0700

    Add files via upload

---
+++ quotes.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Quotes - @Muh.Ikhsan.M</title>
+ <link rel="icon" type="image/png" href="img/profile1.png" sizes="16x16">
+ <link rel="icon" type="image/png" href="img/profile1.png" sizes="32x32">
+</head>	
+<body>
+<style type="text/css">
+body {
+	background-image:url("img/source.gif");
+}
+</style>
+<body bgcolor="black">
+	<audio src="mp3/sunday.mp3" autoplay="autoplay" hidden="hidden">
+</audio>
+
+<?php
+echo "<marquee scrollamount='20' bgcolor='red'><b><font color='white' size=5> >_ </br>";
+echo "Hello noob!<br>";  
+echo "I'm about to learn PHP!<br>"; 
+echo "AKU WES WALEH DISAKITI !</b></center></marquee>"; 
+
+?>
+
+<?php
+echo "<br>";
+echo "<h1> <center><font face='Courier New' color='#ff0000'size=10>My Quotes ;</br></h1></center>";
+echo "<hr>";
+?>
+
+<?php
+echo "<br>";
+echo "<center> <font color='red'> <table width = 850 border =1 bgcolor='white'>
+<tr>
+<td>Jadilah diri sendiri dan Janganlah Suka Merendahkan</td>
+</tr>
+
+<tr>
+<td>Biarlah semua orang membenci-mu, mungkin mereka iri dengan-mu dan janganlah kamu merasa sedih sungguh kamu itu perlu berlatih untuk menjadi seorang penyabar di dalam hidup-mu</td>
+</tr>
+
+<tr>
+<td>Jika Saya Gagal,Maka Saya Akan Memulainya Kembali. Sebab Dalam Kehidupanku, Aku Tidak Kenal Kata Menyerah</td>
+</tr>
+
+<tr>
+<td>Cari musuh lebih mudah daripada cari teman jadi jagalah hubungan pertemanan itu, Janganlah sampai menyesal di kemudian hari.</td>
+</tr> >_
+</table>
+</center>
+</font>";
+?>
+<!-- footer -->
+    <footer class="bg-dark text-white mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <center><p><font color="white">Design &copy; 2020 by <font color="#00FFFF">Muh Ikhsan M</p></center>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+</font>
+</body>
+</html>
